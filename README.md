d56f7afccd2e45c5bba3dedec244824d340979ac
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-09-01 20:10:31 +0200

    :snail:

---
+++ README.md
@@ -0,0 +1,23 @@
+# 🐌
+
+*A Minimal Emoji Link Shortener.*
+
+
+## Install
+
+```bash
+npm i
+```
+
+
+## Start
+
+```bash
+npm start
+```
+
+🐌 starts on Port 1234 by default. Set `PORT` to change default port.
+
+## License
+
+Licensed under the [MIT License](http://opensource.org/licenses/mit-license.php).
--- server.js
+++ server.js
@@ -4,31 +4,33 @@ const emojis = require('emojis-list')
 const validUrl = require('valid-url')
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
-  if (!validUrl.isUri(req.body)) return res.status(400).send('400')
+  if (!validUrl.isUri(req.body.link)) return res.status(400).send('400')
 
-  let emojilink
   let count = 0
+  db.forEach(function (key, val) { count += 1 })
+  count = Math.floor(count / emojis.length)
+
+  let emojiLink = ''
   do {
-    count++
-    emojilink = emojis[Math.random() * emojis.length | 0]
-    if (count >= emojis.length) {
-      emojilink += emojis[Math.random() * emojis.length | 0]
+    for (let i = count; i >= 0; i--) {
+      emojiLink += emojis[Math.random() * emojis.length | 0]
     }
-  } while (db.get(emojilink))
+  } while (db.get(emojiLink))
 
-  console.log(req.body, emojilink)
+  console.log(req.body.link, '>', emojiLink)
 
-  db.set(emojilink, req.body)
-  res.send(emojilink)
+  db.set(emojiLink, req.body.link)
+  res.send(emojiLink)
 })
 
 app.get('/:l', (req, res) => {
   let link = db.get(req.params.l)
 
-  console.log(req.params.l, link)
+  console.log(req.params.l, '>', link)
 
   if (!link) return res.redirect('/')
 
@@ -37,5 +39,4 @@ app.get('/:l', (req, res) => {
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
 
-app.listen(process.env.PORT || 1234, console.log('🐜'))
-
+app.listen(process.env.PORT || 1234, console.log('🐌'))
