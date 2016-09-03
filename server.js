e48f4e56eca92b8fb90054115db31138c2955658
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-09-03 14:40:14 +0200

    :snail: :thumbsup:

--- server.js
+++ server.js
@@ -11,13 +11,9 @@ app.use(bodyParser.json())
 app.post('/', (req, res) => {
   if (!validUrl.isUri(req.body.link)) return res.status(400).send('400')
 
-  let count = 0
-  db.forEach(function (key, val) { count += 1 })
-  count = Math.floor(count / emojis.length)
-
   let emojiLink = ''
   do {
-    for (let i = count; i >= 0; i--) {
+    for (let i = Math.floor(db.size() / emojis.length); i >= 0; i--) {
       emojiLink += emojis[Math.random() * emojis.length | 0]
     }
   } while (db.get(emojiLink))
